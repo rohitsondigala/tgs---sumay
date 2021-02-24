8778c37e7aa9b7aed959da6ff1ceed635f3d1c99
harmit_chavda <chavdaharmit17998@gmail.com>
2021-02-24 13:38:11 +0530

    Response changed in professor list and student list. new fields are added ( rating,notes,reviews count)

--- app/Http/Resources/GetProfessorListResource.php
+++ app/Http/Resources/GetProfessorListResource.php
@@ -21,6 +21,8 @@ class GetProfessorListResource extends JsonResource
         $returnData = array();
         $returnData['uuid'] = $this->user->uuid;
         $returnData['name'] = $this->user->name;
+        $returnData['role']['uuid'] = $this->user->role->uuid;
+        $returnData['role']['title'] = $this->user->role->title;
         $returnData['university_name'] = $this->user->professor_detail->university_name;
         $returnData['college_name'] = $this->user->professor_detail->college_name;
         $returnData['education_qualification'] = $this->user->professor_detail->education_qualification;
@@ -28,11 +30,17 @@ class GetProfessorListResource extends JsonResource
         $returnData['achievements'] = $this->user->professor_detail->achievements;
         $returnData['preferred_language'] = $this->user->professor_detail->preferred_language;
         $returnData['other_information'] = $this->user->professor_detail->other_information;
+        $returnData['stream']['uuid'] = $this->stream->uuid;
+        $returnData['stream']['title'] = $this->stream->title;
         $returnData['subject']['uuid'] = $this->subject->uuid;
         $returnData['subject']['title'] = $this->subject->title;
         $returnData['city']['uuid'] = $this->user->city_detail->id;
         $returnData['city']['title'] = $this->user->city_detail->name;
         $returnData['full_image_path'] = $this->user->full_image_path;
+        $returnData['rating'] = 0;
+        $returnData['total_reviews'] = 0;
+        $returnData['total_notes'] = 0;
+        $returnData['total_queries'] = 0;
         if(in_array($this->subject->uuid,$existingSubjects)){
             $returnData['subject']['is_purchased'] = 1;
         }else{
--- app/Http/Resources/GetStudentListResource.php
+++ app/Http/Resources/GetStudentListResource.php
@@ -18,15 +18,23 @@ class GetStudentListResource extends JsonResource
         $returnData = array();
         $returnData['uuid'] = $this->user->uuid;
         $returnData['name'] = $this->user->name;
+        $returnData['role']['uuid'] = $this->user->role->uuid;
+        $returnData['role']['title'] = $this->user->role->title;
         $returnData['university_name'] = $this->user->student_detail->university_name;
         $returnData['college_name'] = $this->user->student_detail->college_name;
         $returnData['preferred_language'] = $this->user->student_detail->preferred_language;
         $returnData['other_information'] = $this->user->student_detail->other_information;
+        $returnData['stream']['uuid'] = $this->stream->uuid;
+        $returnData['stream']['title'] = $this->stream->title;
         $returnData['subject']['uuid'] = $this->subject->uuid;
         $returnData['subject']['title'] = $this->subject->title;
         $returnData['city']['uuid'] = $this->user->city_detail->id;
         $returnData['city']['title'] = $this->user->city_detail->name;
         $returnData['full_image_path'] = $this->user->full_image_path;
+        $returnData['rating'] = 0;
+        $returnData['total_reviews'] = 0;
+        $returnData['total_notes'] = 0;
+        $returnData['total_queries'] = 0;
 
 
 
